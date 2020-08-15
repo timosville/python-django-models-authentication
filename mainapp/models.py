ad9ac2279dc79309852e5bd19cccd62bbef3b534
Timothy Koech <timosville@gmail.com>
2020-08-15 12:43:33 +0300

    Add author foreign key

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db import models
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
